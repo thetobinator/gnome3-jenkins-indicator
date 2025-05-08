9f1335eeed6628b666f239c71c667f7dc84def49
Tobias Opfermann <tobias@igame3d.com>
2025-05-08 19:55:16 +0200

    fixed install script to use sudo and updated comment about how to restart gnome shell

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,7 +1,7 @@
-./build.sh
+sudo ./build.sh
 EXT_DIR=~/.local/share/gnome-shell/extensions/jenkins-indicator@philipphoffmann.de
-rm -f -r $EXT_DIR
-mkdir -p $EXT_DIR
-cp gnome3-jenkins.zip $EXT_DIR
-cd $EXT_DIR && unzip gnome3-jenkins.zip
-echo "Now press ALT-F2 and enter "R" in the prompt to restart the gnome shell"
+sudo rm -f -r $EXT_DIR
+sudo mkdir -p $EXT_DIR
+sudo cp gnome3-jenkins.zip $EXT_DIR
+cd $EXT_DIR && sudo unzip gnome3-jenkins.zip
+echo "Now press ALT-F2 and enter "restart" in the prompt to restart the gnome shell"
